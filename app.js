b501d6457209759adb869351a4ea386ab5acb240
ArgiTavis <arigtaivs@gmail.com>
2020-05-07 01:22:18 -0400

    Added the ability to put character name and image onto the web page.

--- app.js
+++ app.js
@@ -11,20 +11,34 @@ const createCharacters = () =>{
     'Morty Smith': '2',
     'Rick Sanchez': '1',
   };
-  Object.values(characters).map(character => getCharacters(character))
-}
+  return Object.values(characters).map(getCharacters);
+};
 
 const getCharacters = (id) => {
   const url = buildUrl('https://rickandmortyapi.com/api/', 'character/', id);
   fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(addToDom);
 };
+
+const addToDom = (character) =>{
+  const element = document.createElement('div');
+  const imageElement = document.createElement('img');
+  element.innerHTML = character.name;
+  imageElement.setAttribute('src', character.image);
+  document.body.appendChild(element);
+  document.body.appendChild(imageElement);
+};
+
+
+// const concat = (...item) =>{
+//   console.log(item);
+//   return item.reduce((finalString, string) => finalString + string, '');
+// };
+
+
 
 (() => {
   createCharacters();
 })();
-
-
-
 
